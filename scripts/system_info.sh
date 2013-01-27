4ec76d4d6ff1d3fd2d69c525edd4074b8bf9e385
Jan Carreras <inedit00@gmail.com>
2013-01-27 16:15:41 +0100

    Fixed in system_info.sh when the commands did not return any results
    The problem is that sometimes the commands to not return any result but
    we musts have, at least, a \n charater at the end of the line for being able
    to distinc when starts the next metric.

--- scripts/system_info.sh
+++ scripts/system_info.sh
@@ -16,7 +16,7 @@
 # it will not find the deleted item. Please when deleting items from this script
 # check that you delete it from the dashboard template too. Having metrics that
 # are not being used do not harm to anybody, so they can be left here.
-# Important note2: In my laptop this command is executed with in 42 milli seconds,
+# Important note2: In my RaspPI this command is executed in 550 milli seconds,
 # try to keep this time as lower as you can. This will prevent the dashboard to
 # take ages to load. The execution time must be always lower than a second, keep
 # this in mind.
@@ -48,29 +48,29 @@ echo -n "FREE_MEMORY:"
 echo $free
 
 echo -n "DISK_TOTAL:"
-df -h | grep "rootfs.*/$" --color=none  | awk '{print $2}'
+echo `df -h | grep "rootfs.*/$" --color=none  | awk '{print $2}'`
 
 echo -n "DISK_USED:"
-df -h | grep "rootfs.*/$" --color=none  | awk '{print $3}'
+echo `df -h | grep "rootfs.*/$" --color=none  | awk '{print $3}'`
 
 
 echo -n "DISK_FREE:"
-df -h | grep "rootfs.*/$" --color=none  | awk '{print $4}'
+echo `df -h | grep "rootfs.*/$" --color=none  | awk '{print $4}'`
 
 echo -n "UPTIME:"
-uptime  | awk '{print $3}' | tr -d ','
+echo `uptime  | awk '{print $3}' | tr -d ','`
 
 echo -n "LOAD_AVG:"
-uptime  | awk '{print $8, $9, $10}' | tr -d ","
+echo `uptime  | awk '{print $8, $9, $10}' | tr -d ","`
 
 echo -n "PROCESSOR_NAME:"
-cat /proc/cpuinfo  | grep "model name" | sort -u | egrep ":.*$" -ho | tr -d ":"
+echo `cat /proc/cpuinfo  | grep "model name" | sort -u | egrep ":.*$" -ho | tr -d ":"`
 
 echo -n "PROCESSOR_BOGOMITS:"
-cat /proc/cpuinfo  | grep "bogomips" | head -1 | egrep ":.*$" -ho | tr -d ":"
+echo `cat /proc/cpuinfo  | grep "bogomips" | head -1 | egrep ":.*$" -ho | tr -d ":"`
 
 echo -n "PROCESSOR_CURRENT_SPEED:"
-cat /sys/devices/system/cpu/cpu0/cpufreq/scaling_cur_freq
+echo `cat /sys/devices/system/cpu/cpu0/cpufreq/scaling_cur_freq`
 
 echo -n "PROCESSOR_OVERLOCK:"
-cat /boot/config.txt | grep arm_freq --color=none | egrep  [0-9]+ -ho
+echo `cat /boot/config.txt | grep arm_freq --color=none | egrep  [0-9]+ -ho`
