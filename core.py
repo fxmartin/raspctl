bfd3d0738131442476435379d92a700ba5ca7244
Jan Carreras <inedit00@gmail.com>
2013-03-17 00:18:14 +0100

    Changed the path to the real one

--- core.py
+++ core.py
@@ -182,7 +182,7 @@ def service_action(name=None, action=None):
     if name not in helpers._execute("ls /etc/init.d/"):
         return "Error! Service not found!"
 
-    result = helpers._execute("sudo /opt/raspctl/exec.sh service %s %s" % (name, action))
+    result = helpers._execute("sudo /usr/share/raspctl/scripts/exec.sh service %s %s" % (name, action))
     return result if result else "No information returned"
 
 @get('/about')
