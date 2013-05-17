16f48c2c1334293cb4924d64b59611136672a666
Gilberto J?nior <gilberto.junior@gmail.com>
2013-05-17 01:38:02 +0000

    The original code was not showing the correct top cpu process neither top memory usage.
    To test Top CPU, just simulate a high CPU usage with the following command on a shell session: dd if=/dev/zero of=/dev/null
    
    It's not clear how to simulate Memory usage but you should know the processes that use more memory. You could also use the "top" command.
    
    sources:
    http://www.web-manual.net/linux-3/find-out-top-10-cpu-memory-consuming-processes/
    
    http://stackoverflow.com/questions/2925606/how-to-create-a-cpu-spike-with-a-bash-command

---
+++ helpers.py
@@ -0,0 +1,93 @@
+import config
+import os
+import storage
+import subprocess
+
+
+class Dummy(object):
+    def __init__(self, data, text=""):
+        self.data = data
+        self.text = text
+
+        for k, v in data.items():
+            setattr(self, k, v)
+
+    def __getitem__(self, k):
+        return self.data[k]
+
+    def __getattr__(self, k):
+        return self.text
+
+def execute_command(class_, action, extra_params):
+    if config.COMMAND_EXECUTION == False:
+        return "The command execution is NOT available."
+
+    command = filter(lambda x: x['class_'] == class_ and
+                               x['action'] == action,
+                     storage.read('commands'))
+    if not command:
+        return "Command not found"
+
+    command = command[0]
+
+    for key, value in extra_params.items():
+        command['command'] = command['command'].replace("$%s" % key, value)
+
+    subprocess.call(command['command'], shell=True)
+    return "Executing: %s" % command
+
+
+def check_program_is_installed(prg_name):
+    return subprocess.call("which %s" % prg_name, shell=True) == 0
+
+
+def current_tab(tab_name):
+    setattr(config, "CURRENT_TAB", tab_name)
+
+
+def _execute(cmd):
+    try:
+        output = subprocess.Popen(cmd.split(), stdout=subprocess.PIPE).communicate()
+        return output[0]
+    except OSError:
+        return ""
+
+class Player():
+    def is_installed(self):
+        return check_program_is_installed('mpd') and check_program_is_installed('mpc')
+
+    def play(self, song):
+        _execute("mpc clear")
+        _execute("mpc add " + song)
+        _execute("mpc play 1")
+
+    def stop(self):
+        _execute("mpc clear")
+
+    def volume(self, volume):
+        _execute("mpc volume " + volume)
+
+player = Player()
+
+
+# Yep, I like extremely long and descriptive names for
+# functions and variables (if you haven't noticed it yet) =)
+def execute_system_information_script():
+    # Ok, let's explain this a little bit. The script system_info.sh gets information
+    # about the system and throw the results to the STDOUT with a key-value format.
+    # From here we execute the mentioned script and load it in Python Dictionary
+    # dinamically, so, the names you use in the script for identifying a information
+    # will be the same in the Python code. Please take a look to the comments of the
+    # mentioned file for further information.
+    result = _execute(os.getcwd() + "/scripts/system_info.sh")
+    info = {}
+    for line in result.split('\n'):
+        try:
+            constant, value = line.split(':', 1)
+            info[constant] = value
+        except ValueError:
+            pass
+    return info
+
+def is_tab_active(tabname):
+    return 'active' if config.CURRENT_TAB == tabname else ''
---
+++ views/system_info.tpl
@@ -0,0 +1,92 @@
+<div class="span4">
+	<h3 class="text-info"> Basic </h3>
+	<dl class="dl-horizontal">
+		<dt>Hostname</dt>
+		<dd>{{info['HOSTNAME']}}</dd>
+		<dt>IP address</dt>
+		<dd>{{info['IP_ADDRESS']}}</dd>
+		<dt>Uptime</dt>
+		<dd>{{info['UPTIME']}}</dd>
+	</dl>
+
+	<hr />
+
+	<h3 class="text-info"> Processor </h3>
+	<dl>
+		<dt>CPU Name</dt>
+		<dd>{{info['PROCESSOR_NAME'].strip() or info['PROCESSOR_NAME2']}}</dd>
+		<dt>Bogomits</dt>
+		<dd>{{info['PROCESSOR_BOGOMITS']}}</dd>
+		<dt>Current speed (Hz)</dt>
+		<dd>{{info['PROCESSOR_CURRENT_SPEED']}}</dd>
+		<dt>Overcloked speed (MHz)</dt>
+		<dd>{{info['PROCESSOR_OVERLOCK'].strip() or '--'}}</dd>
+		<dt>Load Average</dt>
+		<dd>{{info['LOAD_AVG']}}</dd>
+	</dl>
+
+	<hr />
+
+	<h3 class="text-info">Top CPU processes</h3>
+	<dl class="dl-horizontal">
+		% for process in info['TOP_PROCESSES'].split('#'):
+			% if process:
+				% usage, pid, name = process.split(' ', 2)
+				<dt title="{{name}}">{{name}}</dt>
+				<dd>PID: {{pid}} - {{usage}}%</dd>
+			% end
+		% end
+	</dl>
+
+	<hr />
+
+	<h3 class="text-info">Top MEMORY processes</h3>
+	<dl class="dl-horizontal">
+		% for process in info['TOP_MEMORY'].split('#'):
+			% if process:
+				% usage, pid, name = process.split(' ', 2)
+				<dt title="{{name}}">{{name}}</dt>
+				<dd>PID: {{pid}} - {{usage}}%</dd>
+			% end
+		% end
+	</dl>
+
+</div>
+
+<div class="span5">
+	<h3 class="text-info">Memory usage</h3>
+	% used = int(float(info['USED_MEMORY']) / float(info['MEMORY_TOTAL']) * 100)
+	% free = int(float(info['FREE_MEMORY']) / float(info['MEMORY_TOTAL']) * 100)
+	% color = "success" if used < 70 else "warning" if used < 85 else "danger"
+	<div class="progress progress-{{color}}">
+		<div class="bar" style="width: {{used}}%;"></div>
+	</div>
+	<dl class="dl-horizontal">
+		<dt>Total memory</dt>
+		<dd>{{info['MEMORY_TOTAL']}} K</dd>
+		<dt>Used</dt>
+		<dd>{{info['USED_MEMORY']}} K <b>({{used}}%)</b></dd>
+		<dt>Free</dt>
+		<dd>{{info['FREE_MEMORY']}} K ({{free}}%)</dd>
+	</dl>
+
+	<hr />
+
+	<h3 class="text-info">Disk usage</h3>
+	% clean = lambda str: float(filter(lambda x: x.isdigit(), str))
+	% used = int(clean(info['DISK_USED']) / clean(info['DISK_TOTAL']) * 100)
+	% free = int(clean(info['DISK_FREE']) / clean(info['DISK_TOTAL']) * 100)
+	% color = "success" if used < 70 else "warning" if used < 85 else "danger"
+	<div class="progress progress-{{color}}">
+		<div class="bar" style="width: {{used}}%;"></div>
+	</div>
+	<dl class="dl-horizontal">
+		<dt>Total HDD space</dt>
+		<dd>{{info['DISK_TOTAL']}}</dd>
+		<dt>Used</dt>
+		<dd>{{info['DISK_USED']}} <b>({{used}}%)</b></dd>
+		<dt>Free</dt>
+		<dd>{{info['DISK_FREE']}} ({{free}}%)</dd>
+	</dl>
+
+</div>
